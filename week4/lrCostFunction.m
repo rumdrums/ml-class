ff87b65a2f3c0c1c84e1effd4920dbda9cc9246b
James <rum_drums@yahoo.com>
2016-03-31 00:24:25 -0500

    remove comments, add cost function calcs (thus far, repeat of last week)

--- week4/lrCostFunction.m
+++ week4/lrCostFunction.m
@@ -37,12 +37,12 @@ grad = zeros(size(theta));
 %
 
 
-
-
-
-
-
-
+h = sigmoid(X*theta);
+disp("THIS IS H: "), disp(h);
+shift_theta = theta(2:size(theta));
+theta_reg = [0;shift_theta];
+J = (1/m)*(-y' * log(h) - (1 - y)'*log(1-h))+(lambda/(2*m))*theta_reg'*theta_reg;
+grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
 
 
 % =============================================================
