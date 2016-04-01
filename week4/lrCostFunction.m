b073962750793dc0183b245949b5f5ce839aa25d
James <rum_drums@yahoo.com>
2016-04-01 00:20:13 -0500

    reorg

--- week4/lrCostFunction.m
+++ week4/lrCostFunction.m
@@ -36,14 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
 h = sigmoid(X*theta);
-disp("THIS IS H: "), disp(h);
+disp("This is theta: "), disp(theta);
+disp("This is h: "), disp(h);
 shift_theta = theta(2:size(theta));
 theta_reg = [0;shift_theta];
-J = (1/m)*(-y' * log(h) - (1 - y)'*log(1-h))+(lambda/(2*m))*theta_reg'*theta_reg;
+J = (1/m)*(-y'*log(h) - (1-y)'*log(1-h))+(lambda/(2*m))*theta_reg'*theta_reg;
 grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
-
 
 % =============================================================
 
